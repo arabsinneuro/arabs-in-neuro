31ef5fb1b471371d770bcbbe168c633364f8bb36
dalia-nasr <dalianasr.dev@gmail.com>
2025-05-21 23:22:55 +0300

    removed folder

--- arabs-in-neuro/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  trailingSlash: true,
-  distDir: "build",
-  output: 'export',
-};
-
-module.exports = nextConfig;
--- arabs-in-neuro/src/context/LanguageContext.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const LanguageContext = React.createContext({
-  preferredLanguage: 'en',
-  currentContent: {},
-});
-
-export default LanguageContext;
